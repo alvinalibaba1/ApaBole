d8e18c6ee4f6036106136b2431a51d85419d40f1
temp <temp@Jehnsens-MacBook-Pro.local>
2023-01-19 22:03:44 +0700

    Test ApaBole

---
+++ Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("ApaBole")
+    } else if i % 3 == 0 {
+        print("Apa")
+    } else if i % 5 == 0 {
+        print("Bole")
+    }else{
+        print(i)
+    }
+}
